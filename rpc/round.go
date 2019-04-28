b730633d1a2233718089859d638d597dfa8015e8
Vanessa Violet <vanessavioletgithub@gmail.com>
2019-04-29 02:59:41 +0800

    fix null get round RPC response

--- rpc/round.go
+++ rpc/round.go
@@ -23,22 +23,39 @@ func getRoundByNumber(store storage.Store, params []interface{}) (map[string]int
 	if err != nil {
 		return nil, err
 	}
+	head, err := store.ReadRound(node)
+	if err != nil {
+		return nil, err
+	}
+	hash := node
+	start := head.Timestamp
+	end := head.Timestamp
+	references := head.References
 
 	snapshots, err := store.ReadSnapshotsForNodeRound(node, number)
 	if err != nil {
 		return nil, err
 	}
-	rawSnapshots := make([]*common.Snapshot, len(snapshots))
-	for i, s := range snapshots {
-		rawSnapshots[i] = &s.Snapshot
-	}
-	start, end, hash := kernel.ComputeRoundHash(node, number, rawSnapshots)
-	round, err := store.ReadRound(hash)
-	if err != nil {
-		return nil, err
-	}
-	if round.NodeId != node || round.Number != number || round.Timestamp != start {
-		return nil, fmt.Errorf("round malformed %s:%d:%d %s:%d:%d", node, number, start, round.NodeId, round.Number, round.Timestamp)
+	if head.Number == number {
+	} else if len(snapshots) > 0 {
+		rawSnapshots := make([]*common.Snapshot, len(snapshots))
+		for i, s := range snapshots {
+			rawSnapshots[i] = &s.Snapshot
+		}
+		start, end, hash = kernel.ComputeRoundHash(node, number, rawSnapshots)
+		round, err := store.ReadRound(hash)
+		if err != nil {
+			return nil, err
+		}
+		if round == nil {
+			return nil, fmt.Errorf("round malformed %s:%d:%d %s", node, number, start, hash)
+		}
+		if round.NodeId != node || round.Number != number || round.Timestamp != start {
+			return nil, fmt.Errorf("round malformed %s:%d:%d %s:%d:%d", node, number, start, round.NodeId, round.Number, round.Timestamp)
+		}
+		references = round.References
+	} else {
+		return nil, fmt.Errorf("round not found")
 	}
 	return map[string]interface{}{
 		"node":       node,
@@ -46,7 +63,7 @@ func getRoundByNumber(store storage.Store, params []interface{}) (map[string]int
 		"start":      start,
 		"end":        end,
 		"number":     number,
-		"references": round.References,
+		"references": references,
 		"snapshots":  snapshotsToMap(snapshots, nil, false),
 	}, nil
 }
@@ -63,18 +80,29 @@ func getRoundByHash(store storage.Store, params []interface{}) (map[string]inter
 	if err != nil {
 		return nil, err
 	}
+	if round == nil {
+		return nil, errors.New("round not found")
+	}
+	start := round.Timestamp
+	end := round.Timestamp
 
 	snapshots, err := store.ReadSnapshotsForNodeRound(round.NodeId, round.Number)
 	if err != nil {
 		return nil, err
 	}
-	rawSnapshots := make([]*common.Snapshot, len(snapshots))
-	for i, s := range snapshots {
-		rawSnapshots[i] = &s.Snapshot
-	}
-	start, end, chash := kernel.ComputeRoundHash(round.NodeId, round.Number, rawSnapshots)
-	if chash != hash {
-		return nil, fmt.Errorf("round malformed %s:%d:%d:%s %s", round.NodeId, round.Number, round.Timestamp, hash, chash)
+	if round.NodeId == hash {
+	} else if len(snapshots) > 0 {
+		rawSnapshots := make([]*common.Snapshot, len(snapshots))
+		for i, s := range snapshots {
+			rawSnapshots[i] = &s.Snapshot
+		}
+		s, e, h := kernel.ComputeRoundHash(round.NodeId, round.Number, rawSnapshots)
+		if h != hash {
+			return nil, fmt.Errorf("round malformed %s:%d:%d:%s %s", round.NodeId, round.Number, round.Timestamp, hash, h)
+		}
+		start, end = s, e
+	} else {
+		return nil, fmt.Errorf("round malformed %s:%d", round.NodeId, round.Number)
 	}
 	return map[string]interface{}{
 		"node":       round.NodeId,
