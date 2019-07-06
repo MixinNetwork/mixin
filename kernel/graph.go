49960f5d4fdfcd0cf0c004a55ddfe5b6a883f6a7
Cedric Fung <cedric@vec.io>
2019-07-06 13:10:22 +0900

    ensure external reference round no later than snapshot time

--- kernel/graph.go
+++ kernel/graph.go
@@ -92,6 +92,9 @@ func (node *Node) startNewRound(s *common.Snapshot, cache *CacheRound) (*FinalRo
 		return nil, nil
 	}
 	if !node.verifyFinalization(s.Timestamp, s.Signatures) {
+		if external.Timestamp > s.Timestamp+config.SnapshotRoundGap {
+			return nill, fmt.Errorf("external reference later than snapshot time %f", time.Duration(external.Timestamp-s.Timestamp).Seconds())
+		}
 		threshold := external.Timestamp + config.SnapshotReferenceThreshold*config.SnapshotRoundGap*64
 		for _, rounds := range node.Graph.RoundHistory {
 			r := rounds[0]
