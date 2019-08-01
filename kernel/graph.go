9bbb7c8beec81bb714c240f9f3f0e193f61c150b
Cedric Fung <cedric@vec.io>
2019-08-02 00:33:33 +0800

    no need to slice the round history when compare

--- kernel/graph.go
+++ kernel/graph.go
@@ -48,9 +48,6 @@ func (node *Node) startNewRound(s *common.Snapshot, cache *CacheRound) (*FinalRo
 			if r.NodeId == s.NodeId {
 				continue
 			}
-			if len(rounds) > config.SnapshotReferenceThreshold {
-				r = rounds[len(rounds)-config.SnapshotReferenceThreshold]
-			}
 			if threshold < r.Start {
 				return nil, fmt.Errorf("external reference %s too early %f", s.References.External, time.Duration(r.Start-external.Timestamp).Seconds())
 			}
