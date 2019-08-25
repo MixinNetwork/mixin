62befb570ba013036f8f1e2336fea68cd2814ed0
Cedric Fung <cedric@vec.io>
2019-08-26 01:36:05 +0800

    revert back to the old final graph ticker

--- network/sync.go
+++ network/sync.go
@@ -165,9 +165,6 @@ func (me *Peer) syncToNeighborLoop(p *Peer) {
 }
 
 func (me *Peer) getSyncPointOffset(p *Peer) (map[crypto.Hash]*SyncPoint, uint64) {
-	ticker := time.NewTicker(time.Duration(config.SnapshotRoundGap))
-	defer ticker.Stop()
-
 	var offset uint64
 	var graph map[crypto.Hash]*SyncPoint
 	for {
@@ -184,7 +181,7 @@ func (me *Peer) getSyncPointOffset(p *Peer) (map[crypto.Hash]*SyncPoint, uint64)
 			if off > 0 {
 				offset = off
 			}
-		case <-ticker.C:
+		case <-time.After(time.Duration(config.SnapshotRoundGap)):
 			return graph, offset
 		}
 	}
