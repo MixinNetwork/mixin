d04c509649690cb550acfa5e7c1dbf1501c1b525
Cedric Fung <cedric@vec.io>
2020-03-13 11:57:18 +0800

    disable cache load to queue

--- kernel/boot.go
+++ kernel/boot.go
@@ -9,7 +9,6 @@ import (
 func (node *Node) Loop() error {
 	panicGo(node.ListenNeighbors)
 	panicGo(node.CosiLoop)
-	panicGo(node.LoadCacheToQueue)
 	panicGo(node.MintLoop)
 	panicGo(node.ElectionLoop)
 	return node.ConsumeQueue()
