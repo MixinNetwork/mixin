d6ea1716e023d8366ea3342ea87180ad40db0d49
Crossle Song <crosslesong@gmail.com>
2022-09-29 10:57:14 +0400

    basic xdc domain validations

---
+++ domains/xdc/validation.go
@@ -0,0 +1,133 @@
+package xdc
+
+import (
+	"encoding/hex"
+	"fmt"
+	"strings"
+
+	"github.com/MixinNetwork/mixin/crypto"
+	"github.com/MixinNetwork/mixin/domains/ethereum"
+	"golang.org/x/crypto/sha3"
+)
+
+var (
+	XDCChainBase string
+	XDCChainId   crypto.Hash
+)
+
+func init() {
+	XDCChainBase = "b12bb04a-1cea-401c-a086-0be61f544889"
+	XDCChainId = crypto.NewHash([]byte(XDCChainBase))
+}
+
+func VerifyAssetKey(assetKey string) error {
+	if len(assetKey) != 43 {
+		return fmt.Errorf("invalid xdc network asset key %s", assetKey)
+	}
+	if !strings.HasPrefix(assetKey, "xdc") {
+		return fmt.Errorf("invalid xdc network asset key %s", assetKey)
+	}
+	if assetKey != strings.ToLower(assetKey) {
+		return fmt.Errorf("invalid xdc network asset key %s", assetKey)
+	}
+	k, err := hex.DecodeString(assetKey[3:])
+	if err != nil {
+		return fmt.Errorf("invalid xdc network asset key %s", assetKey)
+	}
+	if len(k) != 20 {
+		return fmt.Errorf("invalid xdc network asset key %s", assetKey)
+	}
+	return nil
+}
+
+func VerifyAddress(address string) error {
+	if strings.TrimSpace(address) != address {
+		return fmt.Errorf("invalid xdc network address %s", address)
+	}
+	if len(address) != 43 {
+		return fmt.Errorf("invalid xdc network address %s", address)
+	}
+	if !strings.HasPrefix(address, "xdc") {
+		return fmt.Errorf("invalid xdc network address %s", address)
+	}
+	form, err := formatAddress(address)
+	if err != nil {
+		return fmt.Errorf("invalid xdc network address %s", address)
+	}
+	if form != address {
+		return fmt.Errorf("invalid xdc network address %s", address)
+	}
+	a, err := hex.DecodeString(address[3:])
+	if err != nil {
+		return fmt.Errorf("invalid xdc network address %s %s", address, err.Error())
+	}
+	if len(a) != 20 {
+		return fmt.Errorf("invalid xdc network address %s", address)
+	}
+	return nil
+}
+
+func VerifyTransactionHash(hash string) error {
+	if len(hash) != 66 {
+		return fmt.Errorf("invalid xdc network transaction hash %s", hash)
+	}
+	if !strings.HasPrefix(hash, "0x") {
+		return fmt.Errorf("invalid xdc network transaction hash %s", hash)
+	}
+	if strings.ToLower(hash) != hash {
+		return fmt.Errorf("invalid xdc network transaction hash %s", hash)
+	}
+	h, err := hex.DecodeString(hash[2:])
+	if err != nil {
+		return fmt.Errorf("invalid xdc network transaction hash %s %s", hash, err.Error())
+	}
+	if len(h) != 32 {
+		return fmt.Errorf("invalid xdc network transaction hash %s", hash)
+	}
+	return nil
+}
+
+func GenerateAssetId(assetKey string) crypto.Hash {
+	err := VerifyAssetKey(assetKey)
+	if err != nil {
+		panic(assetKey)
+	}
+
+	if assetKey == "xdc0000000000000000000000000000000000000000" {
+		return XDCChainId
+	}
+
+	return ethereum.BuildChainAssetId(XDCChainBase, assetKey)
+}
+
+func formatAddress(to string) (string, error) {
+	var bytesto [20]byte
+	_bytesto, err := hex.DecodeString(to[3:])
+	if err != nil {
+		return "", err
+	}
+	copy(bytesto[:], _bytesto)
+	address := ethereum.Address(bytesto)
+	return addressXdcHex(address), nil
+}
+
+func addressXdcHex(a [20]byte) string {
+	unchecksummed := hex.EncodeToString(a[:])
+	sha := sha3.NewLegacyKeccak256()
+	sha.Write([]byte(unchecksummed))
+	hash := sha.Sum(nil)
+
+	result := []byte(unchecksummed)
+	for i := 0; i < len(result); i++ {
+		hashByte := hash[i/2]
+		if i%2 == 0 {
+			hashByte = hashByte >> 4
+		} else {
+			hashByte &= 0xf
+		}
+		if result[i] > '9' && hashByte > 7 {
+			result[i] -= 32
+		}
+	}
+	return "xdc" + string(result)
+}
