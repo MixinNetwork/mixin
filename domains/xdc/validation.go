17b7038eb297dbe5721b81708d6a9bae0a8e1736
Cedric Fung <cedric@vec.io>
2022-09-29 09:18:11 +0000

    xdc validation re use the ethereum code

--- domains/xdc/validation.go
+++ domains/xdc/validation.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/MixinNetwork/mixin/crypto"
 	"github.com/MixinNetwork/mixin/domains/ethereum"
-	"golang.org/x/crypto/sha3"
 )
 
 var (
@@ -108,26 +107,5 @@ func formatAddress(to string) (string, error) {
 	}
 	copy(bytesto[:], _bytesto)
 	address := ethereum.Address(bytesto)
-	return addressXdcHex(address), nil
-}
-
-func addressXdcHex(a [20]byte) string {
-	unchecksummed := hex.EncodeToString(a[:])
-	sha := sha3.NewLegacyKeccak256()
-	sha.Write([]byte(unchecksummed))
-	hash := sha.Sum(nil)
-
-	result := []byte(unchecksummed)
-	for i := 0; i < len(result); i++ {
-		hashByte := hash[i/2]
-		if i%2 == 0 {
-			hashByte = hashByte >> 4
-		} else {
-			hashByte &= 0xf
-		}
-		if result[i] > '9' && hashByte > 7 {
-			result[i] -= 32
-		}
-	}
-	return "xdc" + string(result)
+	return "xdc" + address.Hex()[2:], nil
 }
