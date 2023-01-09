9357d2e95ba81c131320b733a4cdd4e01e5d45b7
Cedric Fung <cedric@vec.io>
2023-01-09 05:35:18 +0000

    pledge amount test

--- kernel/mint_test.go
+++ kernel/mint_test.go
@@ -16,33 +16,23 @@ import (
 func TestPledgeAmount(t *testing.T) {
 	assert := assert.New(t)
 
-	for b := 0; b < 365; b++ {
-		since := time.Duration(b*24) * time.Hour
-		assert.Equal(common.NewInteger(10000), pledgeAmount(since))
-	}
-	for b := 365; b < 365*2; b++ {
-		since := time.Duration(b*24) * time.Hour
-		assert.Equal(common.NewInteger(11000), pledgeAmount(since))
-	}
-	for b := 365 * 2; b < 365*3; b++ {
-		since := time.Duration(b*24) * time.Hour
-		assert.Equal(common.NewInteger(11900), pledgeAmount(since))
-	}
-	for b := 365 * 3; b < 365*4; b++ {
-		since := time.Duration(b*24) * time.Hour
-		assert.Equal(common.NewInteger(12710), pledgeAmount(since))
-	}
-	for b := 365 * 5; b < 365*6; b++ {
-		since := time.Duration(b*24) * time.Hour
-		assert.Equal(common.NewIntegerFromString("14095.1"), pledgeAmount(since))
-	}
-	for b := 365 * 7; b < 365*8; b++ {
-		since := time.Duration(b*24) * time.Hour
-		assert.Equal(common.NewIntegerFromString("15217.031"), pledgeAmount(since))
-	}
-	for b := 365 * 10; b < 365*11; b++ {
-		since := time.Duration(b*24) * time.Hour
-		assert.Equal(common.NewIntegerFromString("16513.215599"), pledgeAmount(since))
+	for y, m := range map[int]string{
+		0:  "10000",
+		1:  "11000",
+		2:  "11900",
+		3:  "12710",
+		4:  "13439",
+		5:  "14095.1",
+		6:  "14685.59",
+		7:  "15217.031",
+		8:  "15695.3279",
+		9:  "16125.79511",
+		10: "16513.215599",
+	} {
+		for b := 365 * y; b < 365*(y+1); b++ {
+			since := time.Duration(b*24) * time.Hour
+			assert.Equal(common.NewIntegerFromString(m), pledgeAmount(since))
+		}
 	}
 }
 
