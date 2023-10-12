6a2ca74db55db8a77c3b0320587acbe3f1531c6b
Cedric Fung <cedric@vec.io>
2023-10-12 17:47:29 +0000

    maximum value for some special assets

--- storage/badger_asset.go
+++ storage/badger_asset.go
@@ -3,8 +3,23 @@ package storage
 import (
 	"github.com/MixinNetwork/mixin/common"
 	"github.com/MixinNetwork/mixin/crypto"
+	"github.com/MixinNetwork/mixin/domains/bitcoin"
+	"github.com/MixinNetwork/mixin/domains/ethereum"
 	"github.com/dgraph-io/badger/v4"
 )
+
+func assetCapAt(id crypto.Hash) common.Integer {
+	switch id {
+	case bitcoin.BitcoinChainId:
+		return common.NewIntegerFromString("10000")
+	case ethereum.EthereumChainId:
+		return common.NewIntegerFromString("100000")
+	case common.XINAssetId:
+		return common.NewIntegerFromString("1000000")
+	default:
+		return common.NewIntegerFromString("115792089237316195423570985008687907853269984665640564039457.58400791")
+	}
+}
 
 func readTotalInAsset(txn *badger.Txn, hash crypto.Hash) (common.Integer, error) {
 	key := graphAssetTotalKey(hash)
@@ -31,7 +46,12 @@ func writeTotalInAsset(txn *badger.Txn, ver *common.VersionedTransaction) error 
 	case common.TransactionTypeWithdrawalSubmit:
 		total = total.Sub(ver.Outputs[0].Amount)
 	case common.TransactionTypeDeposit:
-		total = total.Add(ver.DepositData().Amount)
+		amount := ver.DepositData().Amount
+		total = total.Add(amount)
+		max := assetCapAt(ver.Asset)
+		if amount.Cmp(max) > 0 || total.Cmp(max) > 0 {
+			panic(amount.String())
+		}
 	default:
 		return nil
 	}
@@ -40,6 +60,6 @@ func writeTotalInAsset(txn *badger.Txn, ver *common.VersionedTransaction) error 
 	return txn.Set(key, []byte(total.String()))
 }
 
-func graphAssetTotalKey(hash crypto.Hash) []byte {
-	return append([]byte(graphPrefixAssetTotal), hash[:]...)
+func graphAssetTotalKey(id crypto.Hash) []byte {
+	return append([]byte(graphPrefixAssetTotal), id[:]...)
 }
