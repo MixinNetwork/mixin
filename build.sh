02799d3e147ed1249abbf2fc8d1e37eddd3f7c1d
Cedric Fung <cedric@vec.io>
2023-10-28 20:15:06 +0000

    fix build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-git checkout config/config.go
-sed -i --  "s/BUILD_VERSION/`git rev-parse HEAD`/g" config/config.go || exit
+git checkout config/reader.go
+sed -i --  "s/BUILD_VERSION/`git rev-parse HEAD`/g" config/reader.go || exit
 CGO_ENABLED=0 go build
