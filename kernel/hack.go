7848c9f75eb042479101917bb9fb309f181ab004
Tristen Blakely <tristenblakely@gmail.com>
2024-01-01 05:46:51 +0000

    fix consensus operation election fork timestamp

--- kernel/hack.go
+++ kernel/hack.go
@@ -1,6 +1,6 @@
 package kernel
 
 const (
-	mainnetConsensusOperationElectionForkAt = uint64(1701098733882642236)
+	mainnetConsensusOperationElectionForkAt = uint64(1701388800000000000)
 	mainnetNodeRemovalHackSnapshotHash      = "b5a9ab66e3b5d24328f8f87bc38e90f0c426dc38413200bb8ecf7f5b8607a5f9"
 )
