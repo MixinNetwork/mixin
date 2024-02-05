b462a3749695ee49907edd21464734ba1253b6bb
Cedric Fung <cedric@vec.io>
2024-02-05 21:53:17 +0000

    fix the test for new network default config

--- config/reader_test.go
+++ config/reader_test.go
@@ -13,7 +13,6 @@ func TestConfig(t *testing.T) {
 	require.Nil(err)
 
 	require.Equal("8bcfad3959892e8334fa287a3c9755fed017cd7a9e8c68d7540dc9e69fa4a00d", custom.Node.Signer.String())
-	require.Equal(false, custom.Node.ConsensusOnly)
 	require.Equal(700, custom.Node.KernelOprationPeriod)
 	require.Equal(4096, custom.Node.MemoryCacheSize)
 	require.Equal(7200, custom.Node.CacheTTL)
@@ -21,8 +20,8 @@ func TestConfig(t *testing.T) {
 	require.Equal(true, custom.Storage.ValueLogGC)
 	require.Equal(7, custom.Storage.MaxCompactionLevels)
 
-	require.Equal("mixin-node.example.com:7239", custom.Network.Listener)
-	require.Len(custom.Network.Peers, 27)
-	require.Equal("43.206.154.20:7239", custom.Network.Peers[26])
+	require.Equal(false, custom.Network.Relayer)
+	require.Len(custom.Network.Peers, 3)
+	require.Equal("5e7ca75239ff68231bd0bcebc8be5b4725e8784b4df8788306c9baa291ec8595@mixin-node1.b1.run:7239", custom.Network.Peers[2])
 	require.Equal(false, custom.RPC.Runtime)
 }
