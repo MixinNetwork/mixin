2517d701d3f8005533496cdac2582b20bdbd4302
Cedric Fung <cedric@vec.io>
2024-07-16 14:41:31 +0000

    basic rpc utxo keys reader implementation

--- rpc/utxo.go
+++ rpc/utxo.go
@@ -7,6 +7,31 @@ import (
 	"github.com/MixinNetwork/mixin/crypto"
 )
 
+type utxoKeysRPCReader struct {
+	rpc string
+}
+
+func NewUTXOKeysRPCReader(rpc string) *utxoKeysRPCReader {
+	return &utxoKeysRPCReader{
+		rpc: rpc,
+	}
+}
+
+func (ur *utxoKeysRPCReader) ReadUTXOKeys(hash crypto.Hash, index uint) (*common.UTXOKeys, error) {
+	utxo := &common.UTXOKeys{}
+	out, err := GetUTXO(ur.rpc, hash.String(), uint64(index))
+	if err != nil || out == nil {
+		return nil, err
+	}
+	utxo.Keys = out.Keys
+	utxo.Mask = out.Mask
+	return utxo, nil
+}
+
+func (ur *utxoKeysRPCReader) ReadDepositLock(deposit *common.DepositData) (crypto.Hash, error) {
+	return crypto.Hash{}, nil
+}
+
 func GetUTXO(rpc, hash string, index uint64) (*common.UTXOWithLock, error) {
 	data, err := callMixinRPC(rpc, "getutxo", []any{hash, index})
 	if err != nil {
